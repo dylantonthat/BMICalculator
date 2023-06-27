62aee6882563130f99d1a1fd931184098ad4fc54
dylantonthat <dylantonthat107@gmail.com>
2023-06-27 13:20:55 -0400

    add Comments

--- src/App.js
+++ src/App.js
@@ -34,31 +34,33 @@ function App() {
 
       // logic for output based on user's height, weight
 
-      if (bmi < 16.0) {
-        setMessage("You are severely underweight.");
-      } else if (bmi >= 16.0 && bmi < 17.0) {
-        setMessage("You are moderately underweight.");
-      } else if (bmi >= 17.0 && bmi < 18.5) {
-        setMessage("You are mildly underweight.");
-      } else if (bmi >= 18.5 && bmi < 25.0) {
-        setMessage("You are at a healthy weight!");
-      } else if (bmi >= 25.0 && bmi < 30.0) {
-        setMessage("You are overweight.");
-      } else if (bmi >= 30.0 && bmi < 35.0) {
-        setMessage("You are Class I Obese.");
-      } else if (bmi >= 35.0 && bmi < 40.0) {
-        setMessage("You are Class II Obese.");
-      } else {
-        setMessage("You are Class III Obese.");
-      }
+      if (bmi < 16.0)
+      { setMessage("You are severely underweight."); }
+      else if (bmi >= 16.0 && bmi < 17.0)
+      { setMessage("You are moderately underweight."); }
+      else if (bmi >= 17.0 && bmi < 18.5)
+      { setMessage("You are mildly underweight."); }
+      else if (bmi >= 18.5 && bmi < 25.0)
+      { setMessage("You are at a healthy weight!"); }
+      else if (bmi >= 25.0 && bmi < 30.0)
+      { setMessage("You are overweight."); }
+      else if (bmi >= 30.0 && bmi < 35.0)
+      { setMessage("You are Class I Obese."); }
+      else if (bmi >= 35.0 && bmi < 40.0)
+      { setMessage("You are Class II Obese."); }
+      else
+      { setMessage("You are Class III Obese."); }
     }
   }
 
   return (
     <div className="app">
       <div className="body">
+
+        {/*Title*/}
         <h1 className="mb-3">Body Mass Index Calculator</h1>
 
+        {/*Display Weight for User Input*/}
         <h4>
           Weight <span>(lbs)</span>:
         </h4>
@@ -69,6 +71,7 @@ function App() {
           onChange={(e) => setWeight(e.target.value)}
         />
 
+        {/*Display Height in Feet, Inches for User Input*/}
         <h4>
           Height <span>(ft, in)</span>:
         </h4>
@@ -85,10 +88,14 @@ function App() {
           onChange={(e) => setInchesHeight(e.target.value)}
         />
 
+        {/*Calculate BMI Button that runs formula using user inputs*/}
         <button onClick={calcBmi}>Compute</button>
 
+        {/*Displays Output*/}
         <h2>BMI: {bmi}</h2>
         <h5>{message}</h5>
+
+        {/*Footer*/}
         <footer className="mt-5">Dylan Ton-That © 2023</footer>
       </div>
     </div>
